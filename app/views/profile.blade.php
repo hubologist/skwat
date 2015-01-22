2d7d6cd7fe10be8611ad68b06ee643182984ec4a
hubologist <hubologist@gmx.com>
2015-01-22 20:38:08 +0000

    Made all routes and get controllers for them. I think.

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -15,19 +15,51 @@
     </div>
 </div>
 @foreach($workouts as $workout)
-<div class="panel panel-default">
-    <div class="panel-body">
-        <h4>{{date("d-M", strtotime($workout->date))}}</h4>
-        <strong>
-            {{$workout->weight}}
-            @if($user->pref_units == 0)
-            kg
-            @else
-            lbs
-            @endif
-            {{$workout->sets}}x{{$workout->reps}}
-        </strong>
+<div class="panel panel-danger">
+    <div class="panel-heading">
+        <div class="panel-title">
+            <h4>{{date("d-M", strtotime($workout->date))}}</h4>    
+        </div>
     </div>
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-md-8">
+                <strong>
+                    {{$workout->weight}}
+                    @if($user->pref_units == 0)
+                    kg
+                    @else
+                    lbs
+                    @endif
+                    {{$workout->sets}}x{{$workout->reps}}
+                </strong> 
+            </div>
+            <div class="col-md-2">
+                <strong>
+                    {{$workout->weight}}
+                    @if($user->pref_units == 0)
+                    kg
+                    @else
+                    lbs
+                    @endif
+                    {{$workout->sets}}x{{$workout->reps}}
+                </strong> 
+            </div>
+            <div class="col-md-2">
+                <strong>
+                    {{$workout->weight}}
+                    @if($user->pref_units == 0)
+                    kg
+                    @else
+                    lbs
+                    @endif
+                    {{$workout->sets}}x{{$workout->reps}}
+                </strong> 
+            </div>
+        </div>
+    </div>
+
+
 </div>
 @endforeach
 @stop
