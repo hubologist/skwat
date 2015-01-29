373acd4ae2ecd5ad6e7b86a0c0e00c125ff36a1a
hubologist <hubologist@gmx.com>
2015-01-29 21:28:30 +0000

    I fucking hate this stupid app.

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -6,9 +6,62 @@
 
 class EntryController extends BaseController {
 
+    public function postEntry()
+    {
+        $validator = Validator::make(Input::all(), array(
+                    'exercise' => 'required',
+                    'reps' => 'required',
+                    'weight' => 'required'
+        ));
+
+        if ($validator->fails())
+        {
+            return Redirect::route('new-entry')
+                            ->withErrors($validator)
+                            ->withInput();
+        }
+        else
+        {
+            // Log the workout
+            if ($user->count())
+            {
+                $user = $user->first();
+
+                // Generate new code and password for the user
+                $code = str_random(64);
+                $password = str_random(8);
+
+                $user->code = $code;
+                $user->password_temp = Hash::make($password);
+
+                if ($user->save())
+                {
+                    // This means the user code has been successfully updated
+                    Mail::send('emails.auth.recover', array(
+                        'link' => URL::route('account-reactivate', $code),
+                        'name' => $user->name,
+                        'password' => $password
+                            ), function($message) use($user) {
+                        $message
+                                ->to($user->email, $user->name)
+                                ->subject('Skwat | Your new password');
+                    });
+
+                    return Redirect::route('home')
+                                    ->with('warning', 'We have sent you a new password by email.');
+                }
+            }
+        }
+        return Redirect::route('account-recovery')
+                        ->with('danger', 'Something went wrong, please try again later.');
+    }
+
     public function getEntry()
     {
-        return View::make('new-entry');
+        $exercises = DB::table('exercises')->get();
+
+        return View::make('new-entry')
+                        ->with('exercises', $exercises);
     }
 
 }
--- app/views/new-entry.blade.php
+++ app/views/new-entry.blade.php
@@ -6,31 +6,25 @@
         <h3 class="form-signin-heading">New entry</h3>
         <div class="row">
             <div class="col-md-12">
-                <strong>Select your lift</strong>
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="radio">
-                            <label>
-                                <input type="radio" name="pref_units" value="kg">Deadlift                              
-                            </label>
-                        </div>
-                        <div class="radio">
-                            <label>
-                                <input type="radio" name="pref_units" value="lbs">Press                              
-                            </label>
-                        </div>
-                        <div class="radio">
-                            <label>
-                                <input type="radio" name="pref_units" value="lbs">Squat                              
-                            </label>
-                        </div>
-                    </div>
+                <strong>Number of Sets</strong>
+                <div class="input-group">
+                    <span class="input-group-btn">
+                        <button type="button" class="btn btn-default btn-number" data-type="minus" data-field="sets">
+                            <span class="glyphicon glyphicon-minus"></span>
+                        </button>
+                    </span>
+                    <span class="input-group-btn">
+                        <button type="button" class="btn btn-default btn-number" data-type="plus" data-field="sets">
+                            <span class="glyphicon glyphicon-plus"></span>
+                        </button>
+                    </span>
+                    <input type="text" name="sets" class="form-control input-number square" value="5" min="1" max="999">
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
-                <strong>Reps</strong>
+                <strong>Number of Reps</strong>
                 <div class="input-group">
                     <span class="input-group-btn">
                         <button type="button" class="btn btn-default btn-number" data-type="minus" data-field="reps">
@@ -61,6 +55,22 @@
                         </button>
                     </span>
                     <input type="text" name="weight" class="form-control input-number square" value="20" min="1" max="999">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <strong>Lift</strong>
+                <div class="row">
+                    @foreach($exercises as $exercise)
+                    <div class="col-sm-6">
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="exercise" value="{{$exercise->name}}">{{$exercise->name}}                              
+                            </label>
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
